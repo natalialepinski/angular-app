47e0732ea0469fe93678a7831379f1ea0bfa4c41
Natalia Lepinski <nlepinski@Natalias-MacBook-Air.local>
2019-02-17 20:06:36 +0000

    add httpClientModule

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,14 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [
-    {
-      url: 'https://vetstreet-brightspot.s3.amazonaws.com/55/77/a059e49243609b3d7cf69a45a302/puppy-thinkstock-173329848-335sm71913.jpg',
-      description: 'puppy1'
-    },
-    {
-      url: 'https://usercontent2.hubstatic.com/12623093_f520.jpg',
-      description: 'puppy2'
-    }
-  ]
+  photos = [];
+
+  constructor(http: HttpClient) {
+    console.log(http);
+  }
 }
